a86091bf2864b95f1bc1e9f90cba500163a59f83
jacker1342 <85124136+jacker1342@users.noreply.github.com>
2023-07-01 02:02:00 +0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,13 +1,10 @@
 /************PROCESS DATA TO/FROM Client****************************/
 
-
 var socket = io(); //load socket.io-client and connect to the host that serves the page
 window.addEventListener('load', function () {
-    document.addEventListener("mouseup", ReportMouseUp, false);
     document.addEventListener("mousedown", ReportMouseDown, false);
-    document.addEventListener("touchmove", TouchMove, false);
+    document.addEventListener("input", PwmInput, false);
 });
-
 
 let curTemp = document.querySelector('.temp_value');
 let curHum = document.querySelector('.hum_value');
@@ -16,6 +13,71 @@ socket.on('change_value', data => {
     curTemp.textContent = data.temp;
     curHum.textContent = data.hum;
 })
+
+// PWM Process
+
+var slider6 = document.getElementById("PWM6");
+var output6 = document.getElementById("val6");
+socket.on('PWM6T', function (data) {
+    document.getElementById('PWM6').value = data;
+    var slider6 = document.getElementById("PWM6");
+    var output6 = document.getElementById("val6");
+    output6.innerHTML = slider6.value;
+    slider6.oninput = function () {
+        output6.innerHTML = this.value;
+    }
+});
+
+var slider13 = document.getElementById("PWM13");
+var output13 = document.getElementById("val13");
+socket.on('PWM13T', function (data) {
+    document.getElementById('PWM13').value = data;
+    var slider13 = document.getElementById("PWM13");
+    var output13 = document.getElementById("val13");
+    output13.innerHTML = slider13.value;
+    slider13.oninput = function () {
+        output13.innerHTML = this.value;
+    }
+});
+
+var slider22 = document.getElementById("PWM22");
+var output22 = document.getElementById("val22");
+socket.on('PWM22T', function (data) {
+    document.getElementById('PWM22').value = data;
+    var slider22 = document.getElementById("PWM22");
+    var output22 = document.getElementById("val22");
+    output22.innerHTML = slider22.value;
+    slider22.oninput = function () {
+        output22.innerHTML = this.value;
+    }
+});
+
+var slider27 = document.getElementById("PWM27");
+var output27 = document.getElementById("val27");
+socket.on('PWM27T', function (data) {
+    document.getElementById('PWM27').value = data;
+    var slider27 = document.getElementById("PWM27");
+    var output27 = document.getElementById("val27");
+    output27.innerHTML = slider27.value;
+    slider27.oninput = function () {
+        output27.innerHTML = this.value;
+    }
+});
+
+
+
+function PwmInput(e) {
+    if (e.target.id === "PWM6") {
+        socket.emit('PWM6', slider6.value);
+    } else if (e.target.id === "PWM13") {
+        socket.emit('PWM13', slider13.value);
+    } else if (e.target.id === "PWM22") {
+        socket.emit('PWM22', slider22.value);
+    } else if (e.target.id === "PWM27") {
+        socket.emit('PWM27', slider27.value);
+    }
+}
+
 
 // Send GPIO button toggle to server
 socket.on('GPIO6', function (data) {
@@ -75,21 +137,3 @@ function ReportMouseDown(e) {
     }
 }
 
-function ReportMouseUp(e) {
-    if (e.target.id === "GPIO6M") {
-        socket.emit("GPIO6", 0);
-        document.getElementById('GPIO6').checked = 0;
-    } else if (e.target.id === "GPIO13M") {
-        socket.emit("GPIO13", 0);
-        document.getElementById('GPIO13').checked = 0;
-    } else if (e.target.id === "GPIO22M") {
-        socket.emit("GPIO22", 0);
-        document.getElementById('GPIO22').checked = 0;
-    } else if (e.target.id === "GPIO27M") {
-        socket.emit("GPIO27", 0);
-        document.getElementById('GPIO27').checked = 0;
-    }
-}
-function TouchMove(e) {
-
-}
